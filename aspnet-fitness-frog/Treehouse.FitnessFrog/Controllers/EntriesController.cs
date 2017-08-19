c5a3787d29829c2c021b0e837e39ecd96218a0a2
Erwan Ahmad Ardiansyah <ardiansyaherwan40@gmail.com>
2017-08-19 15:59:50 +0700

    Apps Fitness Frog
    Application Fitness Frog Test

---
+++ aspnet-fitness-frog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Treehouse.FitnessFrog.Data;
+using Treehouse.FitnessFrog.Models;
+
+namespace Treehouse.FitnessFrog.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private EntriesRepository _entriesRepository = null;
+
+        public EntriesController()
+        {
+            _entriesRepository = new EntriesRepository();
+        }
+
+        public ActionResult Index()
+        {
+            List<Entry> entries = _entriesRepository.GetEntries();
+
+            // Calculate the total activity.
+            double totalActivity = entries
+                .Where(e => e.Exclude == false)
+                .Sum(e => e.Duration);
+
+            // Determine the number of days that have entries.
+            int numberOfActiveDays = entries
+                .Select(e => e.Date)
+                .Distinct()
+                .Count();
+
+            ViewBag.TotalActivity = totalActivity;
+            ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
+
+            return View(entries);
+        }
+        public ActionResult Add()
+        {
+            ViewBag.SelectListItem = new SelectList(Data.Data.Activities, "Id", "Name");
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.AddEntry(entry);
+                return RedirectToAction("Index");
+            }
+            populateSelectedList();
+            return View(entry);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = _entriesRepository.GetEntry((int)id);
+
+            populateSelectedList();
+            return View(entry);
+        }
+        [HttpPost]
+        public ActionResult Edit(Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.AddEntry(entry);
+                return RedirectToAction("Index");
+            }
+            populateSelectedList();
+            return View(entry);
+        }
+        
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Entry entry = _entriesRepository.GetEntry((int)id);
+
+            return View(entry);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            _entriesRepository.DeleteEntry(id);
+
+            return RedirectToAction("Index");
+
+        }
+        private void populateSelectedList()
+        {
+            ViewBag.SelectListItem = new SelectList(Data.Data.Activities, "Id", "Name");
+        }
+    }
+}
